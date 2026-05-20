158841acd22b259e415bd27a5566d43a92cb3ce3
kyleochata <contact.kyle.e_swe@proton.me>
2026-05-20 13:48:20 -0700

    extract name from pt ok src/resource/patient.py @human_name

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from src.resource.patient import get_patient_from_server, summarize_pt_fields, human_name
+
 def main():
     print("Hello from fhirtextualhr!")
+    x = get_patient_from_server("1")
+    print(summarize_pt_fields(x))
+    print(human_name(x))
 
 
 if __name__ == "__main__":
--- src/resource/patient.py
+++ src/resource/patient.py
@@ -1,4 +1,27 @@
 from fhir.resources.patient import Patient
+from ..fhir_client.client import  get_resource
+from typing import Dict, Any, List
+def get_patient_from_server(patient_id: str) -> Patient:
+    pt_dict = get_resource("Patient", patient_id)
+    patient = Patient.model_validate(pt_dict)
+    return patient
 
+def summarize_pt_fields(pt: Patient) -> List[str]:
+    return pt.summary_elements_sequence()
 
+def human_name(patient: Patient) -> str:
+    if not patient.name:
+        return ""
+    name = patient.name[0]
 
+    for n in patient.name:
+        if n.use == "official":
+            name = n
+            break
+    
+    given = ""
+    if name.given:
+        given = " ".join(g for g in name.given if g is not None)
+    family = name.family or ""
+
+    return f"{given} {family}".strip()
